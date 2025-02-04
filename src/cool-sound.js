26cc1bfc9f1220e3e6d5a7b7921047983c16bb80
David Granqvist <david.granqvist@gmail.com>
2025-02-04 22:13:05 +0100

    Song is coming

--- src/cool-sound.js
+++ src/cool-sound.js
@@ -1,37 +1,74 @@
-const context = new (window.AudioContext || window.webkitAudioContext)();
+(() => {
+  const context = new (window.AudioContext || window.webkitAudioContext)();
 
-const notes = {
-  C: 16.35,
-  "C#": 17.32,
-  D: 18.35,
-  "D#": 19.45,
-  E: 20.6,
-  F: 21.83,
-  "F#": 23.12,
-  G: 24.5,
-  "G#": 25.96,
-  A: 27.5,
-  "A#": 29.14,
-  B: 30.87,
-};
+  const notes = {
+    C: 16.35,
+    "C#": 17.32,
+    D: 18.35,
+    "D#": 19.45,
+    E: 20.6,
+    F: 21.83,
+    "F#": 23.12,
+    G: 24.5,
+    "G#": 25.96,
+    A: 27.5,
+    "A#": 29.14,
+    B: 30.87,
+  };
 
-function playTone(note, octave, start, duration) {
-  const osc = context.createOscillator();
-  osc.type = "sine";
-  osc.frequency.value = notes[note] * 2 ** octave;
-  osc.connect(context.destination);
-  osc.start(context.currentTime + start);
-  osc.stop(context.currentTime + start + duration);
-}
+  function playTone(note, octave, start, duration) {
+    const osc = context.createOscillator();
+    osc.type = "sawtooth";
+    if (note === "_") {
+      // Pause
+    } else {
+      osc.frequency.value = notes[note] * 2 ** octave;
+      osc.connect(context.destination);
+      osc.start(context.currentTime + start);
+      osc.stop(context.currentTime + start + duration);
+    }
+  }
 
-const melody = "C4 D4/8 E4 C#4/16";
+  const melody = [
+    // Ja, må hen leva
+    "D#4/4 D#4/8. D#4/16 D#4/4 A#3/4",
+    // Ja, må hen leva
+    "G4/4 G4/8. G4/16 G4/4 D#4/4",
+    // Ja, må hen leva uti
+    "A#4/4 A#4/8. A#4/16 C5/8 A#4/8 G#4/8 G4/8",
+    // hundrade år.
+    "G4/4 F4/8. F4/16 F4/4 _0/4",
+    // Ja, må hen leva
+    "G#4/4 G#4/8. G4/16 F4/4 F4/4",
+  ].join(" ");
+  const noteLengthMultiplier = 2;
 
-const decodedMelody = melody.split(" ").map((item, index) => {
-  const parts = /^([A-G]#?)(\d)(\/\d+)?/.exec(item).slice(1);
-  let [note, octave, noteLength] = parts;
-  noteLength = noteLength ?? "/4";
-  console.log("Note is: ", parts, note, octave, noteLength);
-  return { note, octave, noteLength, index };
-});
+  let totalDuration = 0;
+  const decodedMelody = melody.split(" ").map((item, index) => {
+    console.log("Item is: ", item);
+    const parts = /^([_A-G]#?)(\d)(\/\d+.?)?/.exec(item).slice(1);
+    let [note, octave, noteLength] = parts;
+    let modifier = 1;
+    if (noteLength.endsWith(".")) {
+      noteLength = noteLength.slice(0, -1);
+      modifier = 1.5;
+    }
+    // octave = +octave + 1;
 
-// playTone(note, parseInt(octave), index, 1);
+    noteLength =
+      noteLengthMultiplier *
+      (1 / (noteLength ? +noteLength.substring(1) : 4)) *
+      modifier;
+    console.log("Note is: ", item, parts, note, octave, noteLength);
+    const start = totalDuration;
+    const micropause = 1 / 64;
+    totalDuration += noteLength + micropause;
+    return { note, octave, noteLength, index, start };
+  });
+
+  console.log("Decoded melody: ", decodedMelody);
+
+  for (const { note, octave, start, noteLength } of decodedMelody) {
+    playTone(note, +octave, start, noteLength);
+  }
+})();
